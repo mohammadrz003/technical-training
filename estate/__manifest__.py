df33e816affa1346734ae31da1a32c78cde43ce3
Mohammad Rezaei <rezaiimohammad003@gamil.com>
2023-08-06 11:00:27 +0330

    added security access csv file

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,9 +3,7 @@
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": [
-
-    ],
+    "data": ["security/ir.model.access.csv"],
     "installable": True,
-    'license': 'LGPL-3',
+    "license": "LGPL-3",
 }
